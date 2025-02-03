a95b15fc3b6848c71f30ee358d252202f74dd4a8
A-matheus123 <arteprivada.220watts@gmail.com>
2025-02-03 14:53:37 +0000

    acrescentei a densidade popul. e o PIB per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,13 +4,15 @@ int main() {
     
 //Comecei pelas variáveis:
 
-    char estado = "A"; //Usei o char (que indica apenas um caractere) para indicar o estado.
-    char codigoc[10] = "A01"; //Usei o char "nome"[] (que indica uma cadeia de caracteres) para indicar o código da cidade.
-    char nomecidade[50] = "Brasília"; //Usei novamente o char "nome"[] (que indica uma cadeia de caracteres) para indicar o nome da cidade.
-    float populacao = 2.817; //Usei o float (que indica um número flutuante) para indicar a população, pois quando eu coloquei int (para número inteiro) eu não estava conseguindo interagir no scanf da área. População está em milhões.
-    float area = 5779.14; //Usei o float (que indica um número flutuante) novamente para indicar a área da cidade em Km2.
-    float pib = 328.8; //Usei mais uma vez o float (que indica um número flutuante) para indicar o PIB da cidade em bilhões de reais.
-    int pturisticos = 32; //Usei o int (que indica números inteiros) para indicar o número de pontos turísticos.
+    char estado; //Usei o char (que indica apenas um caractere) para indicar o estado.
+    char codigoc[10]; //Usei o char "nome"[] (que indica uma cadeia de caracteres) para indicar o código da cidade.
+    char nomecidade[50]; //Usei novamente o char "nome"[] (que indica uma cadeia de caracteres) para indicar o nome da cidade.
+    float populacao; //Usei o float (que indica um número flutuante) para indicar a população, pois quando eu coloquei int (para número inteiro) eu não estava conseguindo interagir no scanf da área. População está em milhões.
+    float area; //Usei o float (que indica um número flutuante) novamente para indicar a área da cidade em Km2.
+    float pib; //Usei mais uma vez o float (que indica um número flutuante) para indicar o PIB da cidade em bilhões de reais.
+    int pturisticos; //Usei o int (que indica números inteiros) para indicar o número de pontos turísticos.
+    float denspopul; //No nível intermediário eu acrescentei essa variável para poder calcular densidade populacional;
+    float pibpercap; //Aqui acrescentei essa variável para poder calcular o PIB per capita;
 
     //Variáveis prontas, agora começo a interagir com o usuário usando a combinação estudada "printf" e "scanf":
 
@@ -35,17 +37,22 @@ int main() {
     printf("Digite o número de pontos turísticos: \n");  //Com esse printf, irá aparecer no terminal o texto escrito.
     scanf("%d", &pturisticos); //Esse scanf permitirá que o usuário interaja com o programa através do terminal.
 
-    //Depois que o usuário respondeu através do terminal, eu uso novamente a função printf para imprimir os dados digitados pelo usuário:
+    //Depois que o usuário respondeu através do terminal, eu começo a desenvolver o nível intermediário, que consiste em usar os dados inseridos pelo usuário para calcular a densidade populacional e o PIB per capita;
+
+    denspopul = (populacao / area); //Aqui usei a variável antes criada para poder dividir a população pela área e assim conseguir a "densidade populacional";
+    pibpercap = (pib / populacao); //Aqui usei a variável antes criada para poder dividir o PIB pela população e assim conseguir o "PIB per capita";
 
     printf("\nEstado: %c \n", estado);
     printf("Código da cidade: %s \n", codigoc);
     printf("Nome da cidade: %s \n", nomecidade);
-    printf("População da cidade: %.2f \n", populacao);
-    printf("Área da cidade: %.2f \n", area);
-    printf("Pib da cidade: %.2f \n", pib);
+    printf("População da cidade: %.2f milhões de habitantes \n", populacao);
+    printf("Densidade populacoinal: %.2f por Km2 \n", denspopul); //Imprime a densidade populacional;
+    printf("Área da cidade: %.2f Km2 \n", area);
+    printf("Pib da cidade: %.1f bilhões de reais \n", pib);
+    printf("PIB per capita: %.2f \n", pibpercap); //Imprime o pib per capita;
     printf("Número de pontos turísticos: %d \n \n", pturisticos);
 
-    //E assim termina o cadastro das cartas, espero ter dado tudo que foi pedido nesse desafio!
+    //E assim termina o cadastro das cartas (nível novato), e o acrescento das variáveis para calcular a densidade populacional e o PIB per capita. Espero ter dado tudo que foi pedido nesse desafio!
 
     return 0;
 }
