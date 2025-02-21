716b3c71787b17b1462de1e5f305d04d72f57889
A-matheus123 <arteprivada.220watts@gmail.com>
2025-02-21 16:50:15 +0000

    criei o menu interativo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,6 +14,7 @@ int main() {
     float pibpercap, pibpercap2; //Aqui acrescentei essa variável para poder calcular o PIB per capita;
     long double todasprop, todasprop2; //Variável long float para comparar todas as propriedades;
     float comparacao, comparacao2;
+    int opcao, opcao2;
 
 
     denspopul = (populacao / area); //Densidade baseada na população dividida pela área;
@@ -25,15 +26,21 @@ int main() {
     todasprop = (populacao + area + pib + pturisticos + denspopul + pibpercap); //Soma de todas as propriedades;
     todasprop2 = (populacao2 + area2 + pib2 + pturisticos2 + denspopul2 + pibpercap2);  //Soma de todas as propriedades;
 
+    printf("*** menu principal ***\n");// Aqui eu começo a acrescentar o menu de interação;
+    printf("1. Iniciar o jogo\n");
+    printf("2. Regras do jogo\n");
+    printf("3. Sair do jogo\n");
+    printf("Selecione uma opção: ");
+    scanf("%d", &opcao);
 
-    printf("Digite o código da primeira cidade:\n");//Pede para o usuário inserir o código da primeira cidade;
-    scanf("%s", &codigoc);//Usuário insere o código da primeira cidade (A01);
+    switch (opcao)
+    {
+    case 1://aqui ele diz o que acontece se o usuário selecionar a opção 1;
+        printf("Jogo iniciado\n");
+        printf("Digite o código da primeira cidade:\n");//Pede para o usuário inserir o código da primeira cidade;
+        scanf("%s", &codigoc);//Usuário insere o código da primeira cidade (A01);
 
-    printf("Digite o código da segunda cidade:\n");//Pede para o usuário inserir o código da segunda cidade;
-    scanf("%s", &codigoc2);//Usuário insere o código da primeira cidade;
-
-
-    printf("\nPrimeira carta:\n"); //Imprime "Primeira carta";
+        printf("\nPrimeira carta:\n"); //Imprime "Primeira carta";
 
     printf("Estado: %c\n", estado);//Imprime o código do estado;
     printf("Nome do estado: %s\n", nomeestado);//Imprime o nome do estado;
@@ -46,6 +53,9 @@ int main() {
     printf("PIB per capita: %.2f\n", pibpercap); //Imprime o pib per capita;
     printf("Número de pontos turísticos: %d\n\n", pturisticos);//Imprime o número de pontos turísticos;
 
+        printf("Digite o código da segunda cidade:\n");//Pede para o usuário inserir o código da segunda cidade;
+        scanf("%s", &codigoc2);//Usuário insere o código da segunda cidade (B01);
+
     printf("Segunda carta\n");//Imprime "Segunda carta"
 
     printf("Estado: %c\n", estado2);//Imprime o código do segundo estado;
@@ -57,23 +67,104 @@ int main() {
     printf("Área da cidade: %.2f Km2\n", area2);//Imprime a área da segunda cidade;
     printf("Pib da cidade: %.1f bilhões de reais\n", pib2);//Imprime o PIB da segunda cidade;
     printf("PIB per capita: %.2f\n", pibpercap2); //Imprime o pib per capita;
-    printf("Número de pontos turísticos: %d\n\n", pturisticos2);//Imprime o número de pontos turísticos;
+    printf("Número de pontos turísticos: %d\n\n\n", pturisticos2);//Imprime o número de pontos turísticos;
 
+        printf("Comparações possíveis:\n");
+        printf("1. População;\n");
+        printf("2. Densidade populacional;\n");
+        printf("3. Área;\n");
+        printf("4. PIB;\n");
+        printf("5. PIB per capita;\n");
+        printf("6. Pontos turísticos;\n");
+        printf("7. Comparação geral;\n\n");
+        printf("Escolha um dos números acima: ");
+        scanf("%d", &opcao2);//Aqui eu uso mais uma variável;
 
+        switch (opcao2)
+        {
+        case 1:// O que deve fazer se a opção 1 for selecionada;
+            if (populacao > populacao2)
+            {
+                printf("População da carta 1 é maior do que a carta 2!");
+            } else {
+                printf("População da carta 2 é maior do que a carta 1!");
+            }
+            
+            break;
+        case 2:// O que deve fazer se a opção 2 for selecionada;
+        if (denspopul > denspopul2)
+            {
+                printf("Densidade populacional da carta 1 é maior do que a carta 2, carta 2 ganha!");
+            } else {
+                printf("Densidade populacional da carta 2 é maior do que a carta 1, carta 1 ganha!");
+            }
 
-    printf("\n*Carta com menor densidade populacional:*\n");//Imprime "*Carta com menor densidade populacional:*";
-    if (denspopul < denspopul2){
-        printf("Primeira carta!\n\n");
-    }else {
-        printf("Segunda carta!\n\n");
-    }//Aqui eu usei o "if- else" para imprimir o printf caso seja verdadeiro(Primeira carta vencedora) ou falso(Segunda carta vencedora);
+        break;
+        case 3:// O que deve fazer se a opção 3 for selecionada;
+        if (area > area2)
+            {
+                printf("Área da carta 1 é maior do que a carta 2!");
+            } else {
+                printf("Área da carta 2 é maior do que a carta 1!");
+            }
 
-    printf("*Carta vencedora:*\n\n");//Imprime "*Carta vencedora:*"
-    if (todasprop > todasprop2){
-        printf("***Primeira carta ganhou!!!***\n\n");
-    }else {
-        printf("***Segunda carta ganhou!!!***\n\n");
-    }//Aqui usei o "if- else" para imprimir o printf caso seja verdadeiro(Primeira carta ganhou!!!) ou falso(Segunda carta ganhou!!!);
+        break;
+        case 4:// O que deve fazer se a opção 4 for selecionada;
+        if (pib > pib2)
+            {
+                printf("PIB da carta 1 é maior do que a carta 2!");
+            } else {
+                printf("PIB da carta 2 é maior do que a carta 1!");
+            }
+
+        break;
+        case 5:// O que deve fazer se a opção 5 for selecionada;
+        if (pibpercap > pibpercap2)
+            {
+                printf("PIB per capita da carta 1 é maior do que a carta 2!");
+            } else {
+                printf("PIB per capita da carta 2 é maior do que a carta 1!");
+            }
+
+        break;
+        case 6:// O que deve fazer se a opção 6 for selecionada;
+        if (pturisticos > pturisticos2)
+            {
+                printf("Pontos turísticos da carta 1 é maior do que a carta 2!");
+            } else {
+                printf("Pontos turísticos da carta 2 é maior do que a carta 1!");
+            }
+
+        break;
+        case 7:// O que deve fazer se a opção 7 for selecionada;
+        if (todasprop > todasprop2)
+            {
+                printf("*** CARTA 1 VENCEU O JOGO ***\n");
+            } else {
+                printf("*** CARTA 2 VENCEU O JOGO ***\n");
+            }
+
+        break;
+        default:// O que deve fazer se a opção 1, 2 ou 3 não for selecionada;
+            printf("Digite uma opção válida!");
+        }
+
+        
+        break;
+    case 2://aqui ele diz o que acontece se o usuário selecionar a opção 2;
+        printf("REGRAS DO JOGO:\n");
+        printf("Primeiro digite o código da carta selecionada (A01 ou B01);\n");
+        printf("Escolha qual propriedade você quer comparar;\n");
+        printf("Independente da opção de comparação, o que vai determinar a carta vencedora é a opção 'comparação geral';\n");
+        printf("Você pode jogar quantas vezes você quiser;\n");
+        printf("Divirta-se!!!");
+        break;
+    case 3://aqui ele diz o que acontece se o usuário selecionar a opção 3;
+        printf("Você saiu do jogo.\n");
+        break;
+    default://aqui ele diz o que acontece se o usuário seleecionar uma  opção que não seja 1, 2, ou 3;
+        printf("Opção inválida, digite apenas o número 1, 2 ou 3!");
+    }
 
 
 }
